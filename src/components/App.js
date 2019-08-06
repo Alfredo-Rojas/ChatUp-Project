c2320e6d0bec633855a2959c76089bb2ccdfc741
Alfredo-Rojas <alfredot84@gmail.com>
2019-08-06 11:10:30 -0400

    Basic Set up done, App changed as a component, rendering from Root

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App';
+
+class App extends Component  {
+  render() {
+
+    return (
+      <div>
+         App
+      </div>
+    );
+  }
+  }
+
+export default App;
